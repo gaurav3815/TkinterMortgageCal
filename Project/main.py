c42226bea0674809da575cfccb7a7aa3e32d0ba9
Gaurav Bhandari <bhandarg@coventry.ac.uk>
2021-06-12 16:31:33 +0100

    overwriting calculations

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import tkinter as tk
-import tkinter.font as font
+from tkinter.ttk import *
+
+import time
 
 window = tk.Tk()
 window.title("Mortgage Calculator")
@@ -8,21 +10,35 @@ window.geometry("350x600")
 
 Title = tk.Label(window,text="Calculate Mortgage", font=("Arial", 17), foreground="red").pack(padx=10, pady=10)
 
+counter = 0
 def show_entry_fields():
+    global counter
+    counterCalSet.set(counter)
     DepositCal = int(Deposit.get())/100*int(HousePrice.get())
     DepositCalSet.set(DepositCal)
     MortgageCal = int(HousePrice.get())-int(DepositCal)
-    print(MortgageCal)
     MortgageCalSet.set(MortgageCal)
-
-    tk.Label(window, text="| Total Deposit |", font=("Arial", 12), foreground="red").pack()
-    tk.Label(window, text="", textvariable=DepositCalSet, font=("Arial", 12)).pack()
-    tk.Label(window, text="| Mortgage |", font=("Arial", 12), foreground="red").pack()
-    tk.Label(window, text="", textvariable=MortgageCalSet, font=("Arial", 12)).pack()
-
+    TD = Label(window, text="| Total Deposit |", font=("Arial", 12), foreground="red")
+    TD.pack(pady=5)
+    TDA = Label(window, text="", textvariable=DepositCalSet, font=("Arial", 12))
+    TDA.pack(pady=5)
+    M = Label(window, text="| Mortgage |", font=("Arial", 12), foreground="red")
+    M.pack(pady=5)
+    MA = Label(window, text="", textvariable=MortgageCalSet, font=("Arial", 12))
+    MA.pack(pady=5)
+    counter += 1
+    if counter == 2:
+        TD.destroy()
+        TDA.destroy()
+        M.destroy()
+        MA.destroy()
+        counter = int(0)
+    else:
+        pass
 
 DepositCalSet=StringVar()
 MortgageCalSet=StringVar()
+counterCalSet=StringVar()
 
 tk.Label(window, text="House Price (£)", font=("Arial", 12)).pack(padx=10, pady=10)
 HousePrice = tk.Entry(window)
